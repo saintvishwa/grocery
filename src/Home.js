cfb94651f739c2ef889660c5ced2eef0684dc95e
saintvishwa <90451367+saintvishwa@users.noreply.github.com>
2022-03-28 14:01:13 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,4 @@
+import { logDOM } from '@testing-library/react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
@@ -12,48 +13,54 @@ const Home = () => {
    // const [newOrderList,setNewList] = useState([]);
 
     useEffect(() =>{
-        axios.get(/*("https://browser-buyer.herokuapp.com")||*/"http://localhost:8000").then((response)=>{
-            setOrderList(response.data);
-        });
+        axios.get("http://localhost:8000").then((response)=>{
+            
+            var list2 =  response.data.map(
+                    (d) => (
+                        {...d, select:false })
+                    
+                    );
+                    
+                   setOrderList(list2);
+        
+            });
     },[]);
+    
 
+//for delete items from state and delete api
+    const deleteId =() =>{
+        let arrayIds = [];
+        orderList.forEach(d => {
+            if(d.select) {
+                arrayIds.push(d._id);
+                
+            }
+        });
+        console.log("delete :" + arrayIds);
+
+//for delete state data 
+           let list4 =  orderList.filter( (d)=>{
+                return d.select == false;
+            });
+            console.log("new list :"+list4);
+            setOrderList(list4);
+//api for delete the data in database 
+
+        axios.delete(`http://localhost:8000/delete/${arrayIds}`).then((response) => {
+            if(response.status == 200) {
+                //document.getElementById('delete').innerHTML = 'Order Deleted Successfully';
+            }
+        }).catch(error => 
+            alert(error)
+        );
+    }
     const updateOrder = (id) => {
-        axios.put(/*("https://browser-buyer.herokuapp.com/update")||*/"http://localhost:8000/update",{
+        axios.put("http://localhost:8000/update",{
             buyerOrder:buyerOrder,
             id:id
         });
     };
 
-    const deleteById = () => {
-        let arrayIds = [];
-        orderList.forEach(d => {
-            if(d.select) {
-                arrayIds.push(d._id);
-            }
-        });
-        axios.delete(`http://localhost:8000/delete/${arrayIds}`).then((response) => {
-            if(response.status == 200) {
-                document.getElementById('delete').innerHTML = 'Order Deleted Successfully';
-            }
-        }).catch(error => 
-            alert(error)
-        );
-    };
-
-    orderList.map((d) => {
-        return {
-            select:false,
-            _id:d._id,
-            skuid:d.skuid,
-            orderid:d.orderid,
-            productName:d.productName,
-            origin:d.origin,
-            price:d.price,
-            store1order:d.store1order,
-            store2order:d.store2order,
-            store3order:d.store3order
-        };
-    });
     return (
         <div className='App'>
             <h4 className='page-header'>BUYER DASHBOARD</h4>
@@ -71,30 +78,6 @@ const Home = () => {
                 </div>
             </form><br/>
 
-            <div>
-                <table className='table table-secondary table-bordered table-hover'>
-                    <thead>
-                            <tr>
-                                <th><input type="checkbox" onChange={(event) => {
-                                    let checked = event.target.checked;
-                                    setOrderList(orderList.map((d)=>{
-                                        d.select = checked;
-                                        return d;
-                                    }));
-                                }}/></th>
-                                <th>Date</th>
-                                <th>SKUID</th>
-                                <th>Order ID</th>
-                                <th>Product Name</th>
-                                <th>Origin</th>
-                                <th>Price USD($)</th>
-                                <th>Store 1 Order</th>
-                                <th>Store 2 Order</th>
-                                <th>Store 3 Order</th>
-                                <th>Buyer Order</th>
-                                <th>Actions</th>
-                            </tr>
-                    </thead>
             { orderList.filter((val) => {
                 if(searchProduct === '' && searchId === '') {
                     return val;
@@ -107,16 +90,37 @@ const Home = () => {
                 }
             }).map((val,key) => {
                 return(
+                    <div key={key} className="orders">
+                        <table className='table table-secondary table-bordered table-hover'>
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Date</th>
+                                    <th>SKUID</th>
+                                    <th>Order ID</th>
+                                    <th>Product Name</th>
+                                    <th>Origin</th>
+                                    <th>Price USD($)</th>
+                                    <th>Store 1 Order</th>
+                                    <th>Store 2 Order</th>
+                                    <th>Store 3 Order</th>
+                                    <th>Buyer Order</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
                             <tbody>
-                                <tr key={key}>
-                                    <td><input type="checkbox" onChange={(event) => {
-                                        let checked = event.target.checked;
-                                        setOrderList(orderList.map((data) => {
-                                            if(val._id === data._id){
-                                                data.select = checked;
-                                            }
-                                            return data;
-                                        }));
+                                <tr>
+                                    <td><input type="checkbox" onChange={(e)=>{
+                                        let checked = e.target.checked;
+                                        var list3 =  orderList.map((d) => {
+                                            if(d._id==val._id){ d.select = checked;}
+                                            return d;
+                                            
+                                        });
+                                        console.log(list3);
+                                        setOrderList(list3);
+                                                                                   
+                                            
                                     }} checked={val.select}/></td>
                                     <td>{val.date}</td>
                                     <td>{val.skuid}</td>
@@ -130,19 +134,18 @@ const Home = () => {
                                     <td><input type="number" value={val.buyerOrder} required onChange={(event) => {
                                         setbuyerOrder(event.target.value);
                                     }}/></td>
-                                    <td><button className='btn btn-primary' type='submit' onClick={() => updateOrder(val._id)}>Submit</button></td>
+                                    <td><button className='btn btn-primary' type='submit' onClick={() => updateOrder(val._id)}>Submit Order</button></td>
                                 </tr>
                             </tbody>
+                        </table>
+                    </div>
                 );
             })
             }
-                </table>
-            </div>
             <button className='btn btn-primary' type='submit'>Save Draft</button>
             <a href='/add' className='btn btn-primary'>Add Order</a>
-            <button type='submit' className='btn btn-primary'>Edit order</button>
-            <button type='submit' className='btn btn-primary' onClick={deleteById}>Delete Order</button>
-            <span id='delete' className='text-success'></span>
+            <a href='/edit' className='btn btn-primary'>Edit order</a>
+            <button type='submit' className='btn btn-primary' onClick={deleteId}>Delete</button>
             <button className='btn btn-primary' type='submit'>Logout</button>
         </div>
       );
